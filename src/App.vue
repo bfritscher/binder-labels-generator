785921d33d963e122ef849b29f1e97023cf3fb2a
Boris Fritscher <boris@fritscher.ch>
2018-07-09 11:22:23 +0200

    test of croppa zone sync

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,20 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/print">Print</router-link> |
       <router-link to="/about">About</router-link>
+      <link :href="`https://fonts.googleapis.com/css?family=${selected.title}`" rel="stylesheet" type="text/css">
+      <multiselect v-model="selected" label="title" track-by="title" :options="options" :show-labels="false">
+        <template slot="singleLabel" slot-scope="props">
+          <span class="option__desc"><span class="option__title"  :style="`font-family: '${props.option.title}'`">{{ props.option.title }}</span></span>
+        </template>
+        <template slot="option" slot-scope="props">
+          <link :href="`https://fonts.googleapis.com/css?family=${props.option.title}`" rel="stylesheet" type="text/css">
+          <div class="option__desc"><span class="option__title" :style="`font-family: '${props.option.title}'`">{{ props.option.title }}</span><span class="option__small"></span></div>
+        </template>
+      </multiselect>
     </div>
-     <link :href="`https://fonts.googleapis.com/css?family=${selected.title}`" rel="stylesheet" type="text/css">
-<!--
 
--->
-
-  <multiselect v-model="selected" label="title" track-by="title" :options="options" :show-labels="false">
-    <template slot="singleLabel" slot-scope="props">
-      <span class="option__desc"><span class="option__title"  :style="`font-family: '${props.option.title}'`">{{ props.option.title }}</span></span>
-    </template>
-    <template slot="option" slot-scope="props">
-      <link :href="`https://fonts.googleapis.com/css?family=${props.option.title}`" rel="stylesheet" type="text/css">
-      <div class="option__desc"><span class="option__title" :style="`font-family: '${props.option.title}'`">{{ props.option.title }}</span><span class="option__small"></span></div>
-    </template>
-  </multiselect>
     <router-view/>
   </div>
 </template>
--- src/views/Print.vue
+++ src/views/Print.vue
@@ -4,28 +4,33 @@
         <div class="label" v-for="label in labelsLayout"
         :style="{width: `${label.width}mm` , height: `${label.height}mm`,
         left: `${label.left}mm`, top: `${label.top}mm`}">
-          <image-zone :src="label.image" @change="label.image = $event"></image-zone>
-          <croppa v-model="label.image"></croppa>
+          <croppa v-model="label.image" @image-remove="syncRemove" @zoom="syncZoom(label)" @move="syncMove(label)" @new-image="syncFile(label)"></croppa>
         </div>
       </div>
     </div>
 </template>
 
 <script>
-import ImageZone from "@/components/ImageZone";
 export default {
+  name: "print",
   data() {
     return {
+      masterLabel: null,
       labels: [
         {
           width: 51,
           height: 134,
-          image: ""
+          image: {}
         },
         {
           width: 51,
           height: 134,
-          image: ""
+          image: {}
+        },
+        {
+          width: 51,
+          height: 134,
+          image: {}
         }
       ]
     };
@@ -43,8 +48,55 @@ export default {
       });
     }
   },
-  components: {
-    ImageZone
+  methods: {
+    syncZoom(label) {
+      // startX, startY, scale
+      for (let i = 0; i < this.labelsLayout.length; i++) {
+        const l = this.labelsLayout[i];
+        if (this.labelsLayout.indexOf(label) === 0) {
+          const metadata = label.image.getMetadata();
+          metadata.startX = metadata.startX - i * 400;
+          console.log("zoom", i, metadata);
+          l.image.applyMetadata(metadata);
+        }
+      }
+    },
+    syncMove(label) {
+      // startX, startY, scale
+      const movedIndex = this.labelsLayout.indexOf(label);
+      for (let i = 0; i < this.labelsLayout.length; i++) {
+        const l = this.labels[i];
+        if (l.image !== label.image) {
+          const metadata = label.image.getMetadata();
+          metadata.startX = metadata.startX - (i - movedIndex) * 400;
+          console.log("move", i, metadata);
+          l.image.applyMetadata(metadata);
+        }
+      }
+    },
+    syncFile(label) {
+      if (!this.masterLabel) {
+        this.masterLabel = label;
+      }
+      if (this.masterLabel !== label) {
+        return;
+      }
+      console.log("sync", label);
+      for (let l of this.labels) {
+        if (l.image !== label.image) {
+          l.image._onNewFileIn(label.image.chosenFile);
+        }
+      }
+      setTimeout(() => {
+        this.syncMove(label);
+      }, 1000);
+    },
+    syncRemove() {
+      this.masterLabel = null;
+      for (let l of this.labels) {
+        l.image.remove();
+      }
+    }
   }
 };
 </script>
@@ -61,6 +113,19 @@ export default {
   position: absolute;
   overflow: hidden;
 }
+.croppa-container {
+  background-color: transparent !important;
+}
+.croppa-container:hover {
+  opacity: 1;
+  background-color: #8ac9ef !important;
+}
+
+.croppa-container svg.icon-remove {
+  top: 0 !important;
+  right: 10px !important;
+}
+
 .content {
   position: absolute;
   left: 2cm;
@@ -78,6 +143,17 @@ export default {
   margin-bottom: 0.5cm;
   box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
 }
+
+textarea {
+  width: 100%;
+  height: 200px;
+  text-align: center;
+  font-size: 23pt;
+  overflow: hidden;
+  border: none;
+  resize: none;
+}
+
 html,
 body {
   background-color: grey;
@@ -90,8 +166,21 @@ body {
     box-shadow: none;
     border: none;
   }
+  .croppa-container svg.icon-remove,
   #nav {
     display: none;
+  }
+  ::-webkit-input-placeholder { /* WebKit browsers */
+      color: transparent;
+  }
+  :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+      color: transparent;
+  }
+  ::-moz-placeholder { /* Mozilla Firefox 19+ */
+      color: transparent;
+  }
+  :-ms-input-placeholder { /* Internet Explorer 10+ */
+      color: transparent;
   }
 }
 </style>
